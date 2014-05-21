adea2fcff04ba77605adab9d6d3f4e2aba640dd2
nchanin <nchanin@yahoo.com>
2014-05-20 20:59:06 -0400

    Week 2 Programming Assignment 1: Air Pollution

--- R-Programming/corr.R
+++ R-Programming/corr.R
@@ -1,30 +1,36 @@
+## Noah Chanin
+## 2014-MAY-20
+
+## corr takes in:
+## a directory that has the pollutant data csv files
+## a threshold that indicates the number of completed cases required
+## corr returns:
+## the correlaton between sulfate and nitrate data for ID sets 
+## where the number of completed cases is over the threshold
+
 corr <- function(directory, threshold = 0) {
+  ## set the precision to match samples
   options(digits=4)
-  ## 'directory' is a character vector of length 1 indicating
-  ## the location of the CSV files
-  
-  ## 'threshold' is a numeric vector of length 1 indicating the
-  ## number of completely observed observations (on all
-  ## variables) required to compute the correlation between
-  ## nitrate and sulfate; the default is 0
-  
-  ## Return a numeric vector of correlations
-  
+  ## get the list of csv files
   filenames <- list.files(directory, pattern="*.csv", full.names=TRUE)
+  ## read in all csv files to a dataframe
   specdata <- do.call(rbind, lapply(filenames, read.csv))
 
-  # do some work to be able to filter by threshold
+  ## all records where complete.cases is true
   completed = specdata[complete.cases(specdata), ]
+  ## for all completed cases, calculate the number of observations by ID
   ncos <- with(completed, aggregate((list(nops = ID)), list(ID = ID), length))
+  ## get all of the completed observations where the threshold is over the limit
   result = merge(x = completed, y = ncos[ncos$nops > threshold, ], by.X = "ID", by.y = "ID")
-  
-  # and do the corsource("http://d396qusza40orc.cloudfront.net/rprog%2Fscripts%2Fsubmitscript1.R")
+  ## extract the columns of interest
   work <- result[,c('ID', 'sulfate', 'nitrate')]
+  ## apply the cor function for each ID set and return the results as a vector
   as.vector(sapply(split(work, work$ID), function(X) cor(X$sulfate, X$nitrate, use="complete.obs"), simplify = TRUE))
 }
 
+
+## test data
 # source("corr.R")
-# source("complete.R")
 # cr <- corr("specdata", 150)
 # head(cr)
 # 
